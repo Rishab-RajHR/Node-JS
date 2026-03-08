c764238d97d1e4bd4e262d5d5b01485f6b6e4ed4
Rishab Raj <rr0948555@gmail.com>
2026-03-08 15:05:11 +0530

    Create Server

--- createServer.js
+++ createServer.js
@@ -1,6 +1,7 @@
 const http = require('http');
 
 http.createServer((request,response) => {
+     response.writeHead(200, { 'Content-Type': 'text/html' });
      response.write("<h2>Hello From Node JS</h2>")
      response.end();
 }).listen(1000)
