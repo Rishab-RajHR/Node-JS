65d9b6f70f191b34005f83b789ef0fbfb38faaa0
Rishab Raj <rr0948555@gmail.com>
2026-03-12 13:55:31 +0530

    File

--- showFile.js
+++ showFile.js
@@ -1,2 +1,6 @@
 const fs = require('fs');
-fs.writeFileSync("show.txt", "Node JS")
+// fs.writeFileSync("show.txt", "Node JS")
+const path = require('path');
+
+const myFolder = path.join(__dirname,'myFolder');
+console.log(myFolder)
