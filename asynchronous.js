6f672e9ddcfefefb29dcb154a1733ed3999826f2
Rishab Raj <rr0948555@gmail.com>
2026-03-14 13:47:12 +0530

    Drawback of asynchronous

--- asynchronous.js
+++ asynchronous.js
@@ -1,3 +1,25 @@
-console.log("a")
-console.log("b")
-console.log("c")
+// console.log("a")
+// console.log("b")
+// console.log("c")
+
+
+// Aynchronous programming => that do not block the execution of the code.
+
+// console.log("a")
+// setTimeout(()=>{
+//     console.log("b")
+// },2000)
+// console.log("c")
+
+
+
+
+// NodeJS disadvantage
+
+let firstName = "Alex";
+let lastName = "";
+setTimeout(() => {
+    lastName = "Pandian";
+}, 2000)
+console.log(firstName + " " + lastName)
+// output: Alex because it will not wait for 2 seconds  (solution to it is async/await and promises)
