c5c8c757a03b5c475cca9e4ec6eccbde54d9a071
Rishab Raj <rr0948555@gmail.com>
2026-03-16 12:46:18 +0530

    App

--- express/app.js
+++ express/app.js
@@ -5,4 +5,7 @@ const app = express();
 app.get('', (req,res) => {
     res.send('Good Morning From Express JS')
 })
+app.get('/contact', (req,res) => {
+    res.send('Contact us')
+})
 app.listen(1000);
