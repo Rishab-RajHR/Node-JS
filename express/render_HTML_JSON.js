7d3d44101e8c3bcdce95cb69c3340830ca29646c
Rishab Raj <rr0948555@gmail.com>
2026-03-18 14:09:16 +0530

    Render

--- express/render_HTML_JSON.js
+++ express/render_HTML_JSON.js
@@ -3,7 +3,24 @@ const express = require('express');
 const app = express();
 
 app.get('', (req, res) => {
-     res.send("Hello World")
+     res.send(`
+        <input type="text" placeholder="Your Name" />
+        <a href='/contact'>Go to Contact us Page</a>
+      `)
+})
+
+app.get('/contact', (req, res) => {
+     res.send(`
+        <h2>Hello Express JS</h2>
+         <a href='/about'>Go to About Page</a>
+      `)
+})
+app.get('/about', (req, res) => {
+     res.send([
+       { name: "Alex", address: 'ABC' },
+       { name: "Tovino", address: 'DEF' },
+       { name: "Basil", address: 'GHI' }
+     ])
 })
 
 app.listen(2000);
