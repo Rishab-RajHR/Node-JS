43af5d5bbb1bb3c407ffafd628f578f9718f7048
Rishab Raj <rr0948555@gmail.com>
2026-03-20 13:58:42 +0530

    Hello

---
+++ express/pagenotfound.js
@@ -0,0 +1,21 @@
+const express =  require('express');
+const path = require('path');
+
+const public = path.join(__dirname, "public");
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(`${public}/index.html`)
+})
+
+app.get('/home', (req, res) => {
+    res.sendFile(`${public}/home.html`)
+})
+
+// When page is invalid then * asterik is used
+app.get('*', (req, res) => {
+    res.sendFile(`${public}/pagenotfound.html`)
+})
+
+app.listen(2000);
