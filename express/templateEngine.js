40d9979e22abbbf0bf6061947b7bec16c6f24209
Rishab Raj <rr0948555@gmail.com>
2026-03-20 14:12:28 +0530

    TemplateEngine

---
+++ express/templateEngine.js
@@ -0,0 +1,20 @@
+// ejs => Embedded JavaScript
+
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/welcome', (req,res) => {
+    const empInfo = {
+        name : "Alex",
+        id : '123'
+    }
+    res.render('Welcome', {empInfo})
+})
+
+app.listen(2000, () => {
+    console.log("Server is Running on Port No 2000")
+})
+
