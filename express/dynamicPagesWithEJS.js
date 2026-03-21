e9249ca9ab90f2cd53f69fd2b39a5831707dc28c
Rishab Raj <rr0948555@gmail.com>
2026-03-21 08:39:16 +0530

    Dynamic Pages

--- express/dynamicPagesWithEJS.js
+++ express/dynamicPagesWithEJS.js
@@ -7,9 +7,10 @@ app.set('view engine', 'ejs');
 app.get('/about/:name/:id', (req, res) => {
      const userInfo = {
          name : req.params.name,
-         id : req.params.id
+         id : req.params.id,
+         fav : ['cricket', 'football', 'hockey']
      }
-     res.render('', {userInfo})
+     res.render('About', {userInfo})
 })
 
 app.listen(2000)
