291d96e803399b9dc8a651965381a940fb8b0c22
Rishab Raj <rr0948555@gmail.com>
2026-03-22 14:03:13 +0530

    Middleware

--- express/middlewareConcept.js
+++ express/middlewareConcept.js
@@ -2,6 +2,20 @@ const express = require('express');
 
 const app = express();
 
+const reqFilter = (req, res, next) => {
+    const salary = req.query.salary;
+    if(!salary) {
+        res.send("No Salary Provided");
+    }  
+    else if(salary < 50000) {
+        res.send("Your Salary is below 50000")
+    } else {
+       next();
+    }
+}
+
+app.use(reqFilter);
+
 app.get('/', (req,res) => {
      res.send("This is Our Middleware Tutorial in Express JS")
 })
