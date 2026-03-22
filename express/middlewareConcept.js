38e941e2b94026a16b7db06ebc446e52551b6b2b
Rishab Raj <rr0948555@gmail.com>
2026-03-22 13:50:58 +0530

    Middleware

--- express/middlewareConcept.js
+++ express/middlewareConcept.js
@@ -5,3 +5,10 @@ const app = express();
 app.get('/', (req,res) => {
      res.send("This is Our Middleware Tutorial in Express JS")
 })
+app.get('/about', (req,res) => {
+     res.send("This is Our About Page")
+})
+
+app.listen(2000, () => {
+     console.log("Server is Running on Port No 2000");
+})
