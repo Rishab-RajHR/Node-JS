e3c9303aa35215aba7999bf68542ebdf7a668751
Rishab Raj <rr0948555@gmail.com>
2026-03-23 13:48:39 +0530

    ROuter

---
+++ express/middleware.js
@@ -0,0 +1,12 @@
+const reqFilter = (req, res, next) => {
+    const salary = req.query.salary;
+    if(!salary) {
+       res.send("No Salary Provided")
+    }
+    else if(salary < 50000) {
+       res.send("Your Salary is below 50k")
+    } else {
+        next();
+    }
+}
+module.exports = reqFilter;
--- express/routerMiddleware.js
+++ express/routerMiddleware.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const reqFilter = require('./middleware');
 
 const app = express();
+const route = express.Router();
+route.use(reqFilter);
 
-app.get('/', (req, res) => {
+// app.get('/', reqFilter, (req, res) => {
+//     res.send("This is our Home Page")
+// })
+
+app.get('/',  (req, res) => {
     res.send("This is our Home Page")
 })
-app.get('/contact', (req, res) => {
-    res.send("This is our Home Page")
+route.get('/about', (req, res) => {
+    res.send("This is our About Page")
 })
-app.get('/about', (req, res) => {
-    res.send("This is our Home Page")
+route.get('/contact', (req, res) => {
+    res.send("This is our Contact Page")
 })
+app.use('/', route);
+app.listen(2000);
