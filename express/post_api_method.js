74d64e36c24efdb3c9c2249ad9a1fe296b9d1058
Rishab Raj <rr0948555@gmail.com>
2026-03-28 22:04:26 +0530

    Post API

--- express/post_api_method.js
+++ express/post_api_method.js
@@ -6,10 +6,25 @@ const app = express();
 
 connectDB();
 
-app.post('/employees', (req,res) => {
+app.post('/employees', async (req,res) => {
      try {
         const { name, email, position, department } = req.body;
+
+        const newEmployee = new Employee({
+            name,
+            email,
+            position,
+            department
+        });
+
+        const employee = await newEmployee.save();
+        res.json(employee);
      } catch (error) {
-      
+        console.log("Error saving Data: ", error);
+        res.status(500).send("Server Error");
      }
+});
+
+app.listen(2000, () => {
+    console.log("Server is running on port no 2000")
 })
