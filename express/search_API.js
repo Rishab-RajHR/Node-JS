b58b36e37063619685a4ed61ea2802f7da520cf9
Rishab Raj <rr0948555@gmail.com>
2026-04-01 22:45:20 +0530

    Search API

---
+++ express/search_API.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const connectDB = require('./db/db_connection');
+const Employee = require('./models/employeeModel');
+
+const app = express();
+connectDB();
+
+app.get('/search-emp/:value', async(req,res) => {
+     let searchValue = req.params.value;
+     let result = await Employee.find({
+        "$or" : [
+            {"name" : {$regex : searchValue}}
+        ]
+     });
+     res.send(result);
+})
+
+app.listen(2000, () => {
+    console.log("Server is running on port no 2000");
+})
