babe60743c9ef5f2de40f2d6f7ccb1be0cccf167
Rishab Raj <rr0948555@gmail.com>
2026-04-04 13:10:06 +0530

    JWT

--- express/jwt.js
+++ express/jwt.js
@@ -8,5 +8,37 @@ app.post('/login', (req, res) => {
     const user = {
        username : "Alex",
        email : "alex12@gmail.com"
-    }
+    };
+    const token = jwt.sign({ user }, secretkey, {expiresIn : '500s'});
+    res.json({token})
+})
+
+const verifyToken = (req, res, next) => {
+     const bearerHeader = req.header['authorization'];
+     if(bearerHeader) {
+        const token = bearerHeader.split("")[1];
+        req.token = token;
+        next();
+     }
+     else {
+       res.status(403).json({ result : "Token is not valid"})
+     }
+
+}
+
+app.post('/protected', verifyToken, (req, res) => {
+    jwt.verify(req.token, secretkey, (err, authData) => {
+        if(err) {
+            res.status(403).json({ result : "Unauthorized user"})
+        } else {
+            res.json({
+                message : "You are an authorized user",
+                authData
+            })
+        }
+    })
+})
+
+app.listen(2000, () => {
+    console.log("Server is running on port no 2000")
 })
